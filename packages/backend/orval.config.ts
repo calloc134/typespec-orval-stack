fbc7f2edfe30339b192b43be0d695fe0eb0a90ee
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-08-18 12:29:53 +0900

    バックエンド基盤

---
+++ packages/backend/orval.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  petstore: {
+    output: {
+      mode: "split",
+      target: "src/petstore.ts",
+      client: "hono",
+      override: {
+        hono: {
+          handlers: "src/handlers",
+        },
+        zod: {
+          generate: {
+            body: true,
+            response: true,
+            param: true,
+            header: true,
+            query: true,
+          },
+          strict: {
+            response: true,
+          },
+          // よくわからない部分なので調べる
+          //   preprocess: {
+          //     response: {
+          //       name: "stripNill",
+          //       path: "./src/mutators.ts",
+          //     },
+          //   },
+        },
+      },
+    },
+    input: {
+      target: "../../_generated/@typespec/openapi3/openapi.yaml",
+    },
+  },
+});
