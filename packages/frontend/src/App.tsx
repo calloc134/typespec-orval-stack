2fc03be784b3b00425f71d8363ca282f6c70af82
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-08-25 14:13:07 +0900

    shadcn適用

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export const App = () => {
   return (
     <div className="bg-gray-400">
       <h1 className="text-4xl text-center p-4">Hello, World!</h1>
+      <Button>Click me!</Button>
     </div>
   );
 };
